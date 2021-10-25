b6e70c3912cc8d8be8217aa082a53fcb291c25b4
Congyuwang <congyuwang@link.cuhk.edu.cn>
2021-10-25 10:22:17 +0800

    improve iter_async code quality
    - add implementation comments
    - refactor code

--- src/iter_async.rs
+++ src/iter_async.rs
@@ -7,6 +7,9 @@ use std::sync::Arc;
 use std::thread;
 use std::thread::JoinHandle;
 
+///
+/// This trait implement the async version of `IntoParallelIteratorSync`
+///
 pub trait IntoParallelIteratorAsync<R, T, TL, F>
 where
     F: Send + Clone + 'static + Fn(T) -> Result<R, ()>,
@@ -34,14 +37,20 @@ where
 
 /// iterate through blocks according to array index.
 pub struct ParIterAsync<R> {
-    /// Result receivers, one for each worker thread
-    receiver: Receiver<R>,
+    /// this receiver receives results produced by workers
+    output_receiver: Receiver<R>,
+
     /// handles to join worker threads
     worker_thread: Option<Vec<JoinHandle<()>>>,
-    /// flag to stop workers from fetching new tasks
+
+    /// atomic flag to stop workers from fetching new tasks
     iterator_stopper: Arc<AtomicBool>,
-    /// indicate that workers have all been killed
+
+    /// if this is `true`, it must guarantee that all worker threads have stopped
     is_killed: bool,
+
+    /// number of worker threads
+    worker_count: usize,
 }
 
 impl<R> ParIterAsync<R>
@@ -59,84 +68,88 @@ where
     {
         let cpus = num_cpus::get();
         let iterator_stopper = Arc::new(AtomicBool::new(false));
-        // worker master
-        let (prepare, task) = channel::bounded(MAX_SIZE_FOR_THREAD * cpus);
-        // prepare tasks
-        let sender_thread = thread::spawn(move || {
+        let stopper_clone = iterator_stopper.clone();
+
+        // this thread dispatches tasks to worker threads
+        let (dispatcher, task_receiver) = channel::bounded(MAX_SIZE_FOR_THREAD * cpus);
+        let work_dispatcher = thread::spawn(move || {
             for t in tasks {
-                if prepare.send(t).is_err() {
+                if dispatcher.send(t).is_err() {
                     break;
                 }
             }
         });
-        let mut handles = Vec::with_capacity(cpus + 1);
-        let (sender, receiver) = channel::bounded(MAX_SIZE_FOR_THREAD * cpus);
-        for _ in 0..cpus {
-            let task = task.clone();
-            let iterator_stopper = iterator_stopper.clone();
-            let task_executor = task_executor.clone();
-            let sender = sender.clone();
 
-            // workers
-            let handle = thread::spawn(move || {
-                loop {
-                    if iterator_stopper.load(Ordering::SeqCst) {
-                        break;
-                    }
-                    match get_task(&task) {
-                        // finish
-                        None => break,
-                        Some(task) => match task_executor(task) {
-                            Ok(blk) => {
-                                sender.send(blk).unwrap();
-                            }
-                            Err(_) => {
-                                iterator_stopper.fetch_or(true, Ordering::SeqCst);
-                                break;
-                            }
-                        },
-                    }
+        // output senders for worker threads, and output receiver for user thread
+        let (output_sender, output_receiver) = channel::bounded(MAX_SIZE_FOR_THREAD * cpus);
+
+        // this is what each worker do
+        let worker_task = move || {
+            loop {
+                // check stopper flag, stop if `true`
+                if iterator_stopper.load(Ordering::SeqCst) {
+                    break;
                 }
-            });
-            handles.push(handle);
+
+                // fetch next task
+                match get_task(&task_receiver) {
+                    // break if no more task
+                    None => break,
+                    Some(task) => match task_executor(task) {
+                        Ok(blk) => {
+                            // send output
+                            output_sender.send(blk).unwrap();
+                        }
+                        Err(_) => {
+                            // stop other workers if error is returned
+                            iterator_stopper.fetch_or(true, Ordering::SeqCst);
+                            break;
+                        }
+                    },
+                }
+            }
+        };
+
+        // spawn worker threads
+        let mut worker_handles = Vec::with_capacity(cpus + 1);
+        for _ in 0..cpus {
+            worker_handles.push(thread::spawn(worker_task.clone()));
         }
-        handles.push(sender_thread);
+        worker_handles.push(work_dispatcher);
 
         ParIterAsync {
-            receiver,
-            worker_thread: Some(handles),
-            iterator_stopper,
+            output_receiver,
+            worker_thread: Some(worker_handles),
+            iterator_stopper: stopper_clone,
             is_killed: false,
+            worker_count: cpus,
         }
     }
 }
 
 impl<R> ParIterAsync<R> {
     ///
-    /// stop workers from fetching new tasks, and flush remaining works
-    /// to prevent blocking.
+    /// - stop workers from fetching new tasks
+    /// - pull one result from each worker to prevent `send` blocking
     ///
     pub fn kill(&mut self) {
         if !self.is_killed {
             // stop threads from getting new tasks
             self.iterator_stopper.fetch_or(true, Ordering::SeqCst);
-            // flush the remaining tasks in the channel
-            loop {
-                // wait until all workers have stopped
-                if self.receiver.recv().is_err() {
-                    break;
-                }
+            // receive one for each channel to prevent blocking
+            for _ in 0..self.worker_count {
+                let _ = self.output_receiver.try_recv();
             }
-            // loop break only when task_order is dropped (all workers have stopped)
+            // all workers should reasonably stopped by now
             self.is_killed = true;
         }
     }
 }
 
 ///
-/// A helper function that locks tasks,
-/// register thread_number and task_number
-/// before releasing tasks lock.
+/// A helper function to receive task from task receiver.
+///
+/// It guarantees to return None if and only if there is no more new task.
 ///
 #[inline(always)]
 fn get_task<T>(tasks: &channel::Receiver<T>) -> Option<T>
@@ -157,7 +170,7 @@ impl<R> Iterator for ParIterAsync<R> {
         if self.is_killed {
             return None;
         }
-        match self.receiver.recv() {
+        match self.output_receiver.recv() {
             Ok(block) => Some(block),
             // all workers have stopped
             Err(_) => {
@@ -170,9 +183,9 @@ impl<R> Iterator for ParIterAsync<R> {
 
 impl<R> ParIterAsync<R> {
     ///
-    /// Join worker threads. This can be only called once.
-    /// Otherwise it results in panic.
-    /// This is automatically called in `join()`
+    /// Join worker threads. This can be only called only once.
+    /// Otherwise it will panic.
+    /// This is automatically called in `drop()`
     ///
     fn join(&mut self) {
         for handle in self.worker_thread.take().unwrap() {
